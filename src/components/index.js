f68b987d69bff8d50399758fc69908435aad8668
Doruk Kutlu <dorukk@gmail.com>
2019-01-12 21:37:00 +0300

    <A> and <Img>

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Button } from "./button";
 export { default as H1 } from "./h1";
 export { default as Input } from "./input";
 export { default as TextArea } from "./textarea";
+export { default as A } from "./anchor";
+export { default as Img } from "./img";
